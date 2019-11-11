080aa8da30f8293c843102fc12dafd6ba685e8c1
SssJWww <54054262+SssJWww@users.noreply.github.com>
2019-11-11 16:05:18 +0800

    修改了config文件

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,8 @@ module.exports = {
       '/api': {
         target: 'http://www.91damai.com',
         changeOrigin: true
+        // Axios.post("/api/goods/doyen","page_no: 1").then(res =>{
+        // console.log(res.data)}))
       }
     }
   }
