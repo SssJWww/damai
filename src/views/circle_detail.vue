7f8765af5a0e362f61d6eaa1231af93a442394c2
lgz <2849127860@qq.com>
2019-11-11 22:44:06 +0800

    完成circle—-detail基本样式

--- src/views/circle_detail.vue
+++ src/views/circle_detail.vue
@@ -14,6 +14,37 @@
         </div>
       </div>
     </div>
+    <div class="list">
+      <h3>达人清单</h3>
+      <div class="setion">
+        <div class="sample" v-for="data in datalist" :key="data.id">
+          <img :src="data.pict_url" class="list_img" />
+          <p class="list_title">{{data.title}}</p>
+          <p class="price">
+            <span class="s1">
+              <em>卷后</em>
+              <br />
+              <em class="ff4040">{{data.coupon_after_price}}</em>
+            </span>
+            <span class="s2">
+              <em>佣金</em>
+              <br />
+              <em class="ff4040">{{data.reward_amount}}</em>
+            </span>
+            <span class="s3">
+              <em>营销</em>
+              <br />
+              <em class="ff4040">{{data.commission_rate}}%</em>
+            </span>
+          </p>
+          <div class="quan">
+            <i class="i1">券</i>
+            <i class="i2">￥{{data.coupon_discount}}</i>
+            <i class="i3">已售 {{data.volume}}</i>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -23,7 +54,8 @@ export default {
     return {
       title: null,
       time: null,
-      conent: null
+      conent: null,
+      datalist: []
     }
   },
   mounted () {
@@ -32,6 +64,8 @@ export default {
       this.title = res.data.data.title
       this.time = res.data.data.created_at_str
       this.conent = res.data.data.content
+      this.datalist = res.data.data.user_badge_list
+      console.log(this.datalist)
     })
   }
 }
@@ -43,8 +77,11 @@ export default {
   text-align: center;
   font-size: 0.2rem;
   background-color: #fff;
-  position: relative;
+  position: fixed;
   overflow: hidden;
+  top: 0;
+  left: 0;
+  width: 100%;
   .iconfont {
     font-size: 30px;
     position: absolute;
@@ -57,7 +94,7 @@ export default {
     background-color: #fff;
     overflow: hidden;
     margin: 0 16px;
-    margin-top: 0.18rem;
+    margin-top: 0.7rem;
     .title {
       background-color: #fff;
       overflow: hidden;
@@ -83,6 +120,123 @@ export default {
       }
       .conent {
         background-color: #fff;
+        img {
+          width: 3.43rem !important;
+        }
+      }
+    }
+  }
+}
+.list {
+  height: 1.89rem;
+  margin-top: 0.15rem;
+  h3 {
+    height: 0.46rem;
+    line-height: 0.46rem;
+    color: #ff4040;
+    padding-left: 0.2rem;
+    background-color: #fff;
+    font-weight: 100;
+    border-bottom: 1px solid #eee;
+    border-top: 1px solid #eee;
+  }
+  .setion {
+    height: 1.42rem;
+    position: relative;
+    background-color: #fff;
+    padding: 0.15rem;
+    .sample {
+      height: 1.1rem;
+      background-color: #fff;
+      border: 1px solid #eee;
+      position: relative;
+      .list_img {
+        width: 0.89rem;
+        height: 0.89rem;
+        position: absolute;
+        top: 50%;
+        margin-top: -0.44rem;
+        margin-left: 0.06rem;
+      }
+      .list_title {
+        position: absolute;
+        top: 0.05rem;
+        left: 1.2rem;
+        font-size: 0.16rem;
+        font-weight: 500;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        width: 2.26rem;
+      }
+      .price {
+        position: absolute;
+        top: 0.3rem;
+        left: 1.1rem;
+        .s1,
+        .s2,
+        .s3 {
+          position: absolute;
+          display: block;
+          float: left;
+          width: 0.68rem;
+          height: 0.44rem;
+          text-align: center;
+          em {
+            font-size: 12px;
+            color: #999;
+            font-style: normal;
+          }
+          .ff4040 {
+            color: #ff4040;
+          }
+        }
+        .s1 {
+          top: 0;
+          left: 0;
+        }
+        .s2 {
+          top: 0;
+          left: 0.75rem;
+          border-left: 1px solid #eee;
+          border-right: 1px solid #eee;
+        }
+        .s3 {
+          top: 0;
+          left: 1.43rem;
+        }
+      }
+      .quan {
+        position: absolute;
+        left: 1.2rem;
+        top: 0.75rem;
+        i {
+          font-style: normal;
+          display: inline-block;
+        }
+        .i1 {
+          width: 0.22rem;
+          height: 0.22rem;
+          background-color: #ff4040;
+          color: white;
+          text-align: center;
+          line-height: 0.22rem;
+        }
+        .i2 {
+          width: 0.69rem;
+          height: 0.22rem;
+          text-align: center;
+          background-color: #fff;
+          border: 1px solid #ff4040;
+          line-height: 0.2rem;
+          color: #ff4040;
+          font-weight: 500;
+        }
+        .i3{
+            margin-left: .75rem;
+            font-size: 12px;
+            color: #999
+        }
       }
     }
   }
